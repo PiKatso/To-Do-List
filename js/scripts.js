0e5baab1ea8e4e6d709740e974e58c2c9787030d
Kat Gifford and Tyler Stephenson <tyler@>
2017-04-03 17:00:07 -0700

    add js to add more than one chore list.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@
 
   var createList = function(array) {
     array.forEach(function(item){
-      $(".chore-output ul").append(`<li class="listed-task">${item}</li>`);
+      $(".chore-output:last-child ul").append(`<li class="listed-task">${item}</li>`);
       $(".chore-output ul li").last().click(function() {
           $(this).toggleClass("done");
       });
@@ -14,6 +14,7 @@
   };
 
   var userTaskArray = [];
+
 // User Interface Logic
 $(document).ready(function(){
 
@@ -24,24 +25,24 @@ $(document).ready(function(){
     return userTaskArray.push(userChoreItem);
   });
 
-
-  $("form").submit(function(event) {
+  $("form#list").submit(function(event) {
     event.preventDefault();
     var userChoreName = $("input[name=chore-name]").val();
     var newChore = new toDoList(userChoreName, userTaskArray);
 
     $(".task-output").empty();
-    $(".chore-output h3").text(newChore.choreName);
+    $("#chore-output").append('<div class="chore-output">' +
+                                '<h3></h3>' +
+                                '<ul>  </ul>' +
+                              '</div>');
+    $(".chore-output:last-child h3").text(newChore.choreName);
     createList(userTaskArray);
 
+    $(".chore-output h3").last().click(function() {
+      // $("~ul li", this).removeClass("done");
+      // $("~ul li").toggleClass("done");
+      $(this).toggleClass("done");
+    });
     return userTaskArray = [];
   });
-
-  $(".chore-output h3").click(function() {
-    $(".chore-output").toggleClass("done");
-  });
-
-
-
-
 });
